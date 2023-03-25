6fe2f360e65a1d9eac9a7fde63e763ffcb0ab90e
shuaib shaban <shuaib>
2023-03-25 08:25:34 +0300

    done test

--- app/serializers/movie_summary_serializer.rb
+++ app/serializers/movie_summary_serializer.rb
@@ -2,6 +2,7 @@ class MovieSummarySerializer < ActiveModel::Serializer
   attributes :summary
 
   def summary
-    "#{object.title} - (#{object.description}[0..30])"
+    "#{self.object.title} - #{self.object.description[0..49]}..."
   end
+
 end
